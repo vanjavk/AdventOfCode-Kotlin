05a1033f40fef5373c3f58da9a098a446eda3dec
vanjavk <developer@vanjavk.me>
2020-12-22 07:41:44 +0100

    part 1 solved a bit differently

--- src/main/kotlin/me/vanjavk/Day01.kt
+++ src/main/kotlin/me/vanjavk/Day01.kt
@@ -1,22 +1,34 @@
 package me.vanjavk
 
-import kotlin.reflect.typeOf
+import kotlin.system.measureTimeMillis
+
 
 class Day01(private val input: String) {
     init {
-        println("Day 1 - part 1: ${part1()}")
+        //println("Day 1 - part 1: ${part1()}")
+
+        part1()
+
 //        part2()
     }
 
-    private fun part1(): Int {
+    private fun part1BAD(): Int {
         val complementaryMap = mutableMapOf<Int, Int>()
         input.lines().map {
-            it.toInt().also { complementaryMap[2020 - it] = it  }
+            it.toInt().also { complementaryMap[2020 - it] = it }
         }.forEach {
-            if (complementaryMap.containsKey(it)) return it*(2020-it)
+            if (complementaryMap.containsKey(it)) return it * (2020 - it)
         }
         return -1
     }
+
+    private fun part1(): Int =
+        input.lines().map { it.toInt() }.sorted().run {
+            mapIndexedNotNull { idx, a ->
+                this.firstOrNull { a + it == 2020 }
+                    ?.let { a * it }
+            }.first()
+        }
 }
 
 //            return@map
