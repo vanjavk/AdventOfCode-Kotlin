ae736e41df1dc5b41c636804abd99ccdac57511b
vanjavk <developer@vanjavk.me>
2020-12-22 11:33:39 +0100

    Part 2 completed.

--- src/main/kotlin/me/vanjavk/days/Day01.kt
+++ src/main/kotlin/me/vanjavk/days/Day01.kt
@@ -4,23 +4,46 @@ package me.vanjavk.days
 class Day01(private val input: String) {
     init {
         println("Day 1 - part 1: ${part1()}")
+        println("Day 1 - part 2: ${part2()}")
 //        part1()
 //        part2()
     }
 
-    private fun part1BAD(): Int {
+    private fun part1(): Int {
         val complementaryMap = mutableMapOf<Int, Int>()
         return input.lines().map {
             it.toInt().also { complementaryMap[2020 - it] = it }
-            //        }.forEach {
-            //            if (complementaryMap.containsKey(it)) return it * (2020 - it)
-            //        }
         }.first {
             complementaryMap.containsKey(it)
         }.let { it * (2020 - it) }
     }
 
-    private fun part1(): Int =
+    private fun part2(): Int {
+        val numbers = input.lines().map { it.toInt() }.sorted()
+        val count = numbers.size - 1
+        var left = 0
+        var right = count
+//        println(numbers)
+        while (true) {
+            for (middle in left + 1 until right - 1) {
+                val sum = numbers[left] + numbers[middle] + numbers[right]
+//                println("$left $middle $right $sum ${numbers[left]} ${numbers[middle]} ${numbers[right]}")
+                when {
+                    sum == 2020 -> return numbers[left] * numbers[middle] * numbers[right]
+                    sum > 2020 -> {
+                        right--; break
+                    }
+                    sum < 2020 && middle + 1 == right -> {
+                        left++; break
+                    }
+                }
+            }
+        }
+        return -1
+    }
+
+
+    private fun part1BAD(): Int =
         input.lines().map { it.toInt() }.sorted().run {
             mapIndexedNotNull { _, a ->
                 this.firstOrNull { a + it == 2020 }
